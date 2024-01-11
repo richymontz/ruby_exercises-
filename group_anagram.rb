dbd13c7751159fc89254a19780d54f87fa227ca0
Your Name <ingricardomontes@gmail.com>
2024-01-11 16:07:20 -0500

    Add statement to anagram exercise

--- group_anagram.rb
+++ group_anagram.rb
@@ -1,3 +1,7 @@
+#* Write a Ruby method group_anagrams that takes an array of strings as input 
+#* and groups the anagrams together. 
+#* Anagrams are words or phrases formed by rearranging the letters of another.
+
 def group_anagrams(strs)
   anagrams = Hash.new { |hash, key| hash[key] = [] }
   strs.each do |str|
@@ -8,3 +12,5 @@ def group_anagrams(strs)
 end
 
 puts group_anagrams(['cat', 'tea', 'eat', 'ate', 'bat'])
+puts group_anagrams(["listen", "silent", "enlist", "heart", "earth", "hater"])
+puts group_anagrams(["listen", "silent", "enlist", "heart", "earth", "h@ter"])
