5e0db215afefc6713233ffb6ab741b78e46057e0
Ricardo Montes <ricardo.montes@ideaware.co>
2024-12-18 09:58:41 -0500

    Add improvements to  group_anagram.rb

--- group_anagram.rb
+++ group_anagram.rb
@@ -2,15 +2,34 @@
 #* and groups the anagrams together. 
 #* Anagrams are words or phrases formed by rearranging the letters of another.
 
+
+#sorting chars 
 def group_anagrams(strs)
   anagrams = Hash.new { |hash, key| hash[key] = [] }
+  
   strs.each do |str|
-    sorted_str = str.chars.sort.join
+    sorted_str = str.chars.sort.join #complexity O(n log n)
     anagrams[sorted_str] << str
   end
-  puts anagrams
+  
+  anagrams.values
 end
 
-puts group_anagrams(['cat', 'tea', 'eat', 'ate', 'bat'])
-puts group_anagrams(["listen", "silent", "enlist", "heart", "earth", "hater"])
-puts group_anagrams(["listen", "silent", "enlist", "heart", "earth", "h@ter"])
+#Sorting bytes from chars
+def group_anagrams_bytes(strs)
+  anagrams = Hash.new { |hash, key| hash[key] = [] }
+  
+  strs.each do |str|
+    bytes_array = str.bytes.sort # complexity O(n)
+    anagrams[bytes_array] << str
+  end
+  
+  anagrams.values
+end
+
+p group_anagrams(['cat', 'tea', 'eat', 'ate', 'bat'])
+# OUTPUT: [["cat"], ["tea", "eat", "ate"], ["bat"]]
+p group_anagrams(["abc", "bca", "cab", "aabbcc", "xyz", "zyx"])
+# OUTPUT: [["abc", "bca", "cab"], ["aabbcc"], ["xyz", "zyx"]]
+p group_anagrams(["abc", "bca", "cab", "ad", "da", "bc"])
+# OUTPUT: [["abc", "bca", "cab"], ["ad", "da"], ["bc"]]
